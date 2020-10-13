1c6d48c6bb07309d55ecbe8fdc0bf261e57f7157
Aidan Holland <aidan@censys.io>
2020-10-12 21:46:49 -0400

    chore: rearrange modules

--- install.sh
+++ install.sh
@@ -1,35 +1,69 @@
-mkdir -p ~/.recon-ng/modules/recon/companies-contacts
-cp censys_email_address.py ~/.recon-ng/modules/recon/companies-contacts
+# variables
+RECON_NG=~/.recon-ng
+RECON_NG_MODULES=$RECON_NG/modules
+RECON_NG_RECON_MODULES=$RECON_NG_MODULES/recon
+RECON_NG_KEYS=$RECON_NG/keys.db
 
-mkdir -p ~/.recon-ng/modules/recon/companies-domains
-cp censys_subdomains.py ~/.recon-ng/modules/recon/companies-domains
+# installed check
+if [ ! -d $RECON_NG ]; then
+	echo "[-] Please install and run recon-ng before installing modules."
+	exit 1
+fi
 
-mkdir -p ~/.recon-ng/modules/recon/companies-hosts
-cp censys_org.py ~/.recon-ng/modules/recon/companies-hosts
-cp censys_tls_subjects.py ~/.recon-ng/modules/recon/companies-hosts
+# install context
+echo "[*] Installing censys recon-ng modules..."
+mkdir -p $RECON_NG_RECON_MODULES
 
-mkdir -p ~/.recon-ng/modules/recon/companies-netblocks
-cp censys_company_netname.py ~/.recon-ng/modules/recon/companies-netblocks
+# companies-contacts
+mkdir -p $RECON_NG_RECON_MODULES/companies-contacts
+cp companies-contacts/censys_email_address.py $RECON_NG_RECON_MODULES/companies-contacts
 
-mkdir -p ~/.recon-ng/modules/recon/domains-companies
-cp censys_companies.py ~/.recon-ng/modules/recon/domains-companies
+# companies-domains
+mkdir -p $RECON_NG_RECON_MODULES/companies-domains
+cp companies-domains/censys_subdomains.py $RECON_NG_RECON_MODULES/companies-domains
 
-mkdir -p ~/.recon-ng/modules/recon/domains-hosts
-cp censys_domain.py ~/.recon-ng/modules/recon/domains-hosts
+# companies-hosts
+mkdir -p $RECON_NG_RECON_MODULES/companies-hosts
+cp companies-hosts/censys_org.py $RECON_NG_RECON_MODULES/companies-hosts
+cp companies-hosts/censys_tls_subjects.py $RECON_NG_RECON_MODULES/companies-hosts
 
-mkdir -p ~/.recon-ng/modules/recon/hosts-hosts
-cp censys_query.py ~/.recon-ng/modules/recon/hosts-hosts
+# companies-netblocks
+mkdir -p $RECON_NG_RECON_MODULES/companies-netblocks
+cp companies-netblocks/censys_company_netname.py $RECON_NG_RECON_MODULES/companies-netblocks
 
-mkdir -p ~/.recon-ng/modules/recon/hosts-ports
-cp censys_hostname.py ~/.recon-ng/modules/recon/hosts-ports
-cp censys_ip.py ~/.recon-ng/modules/recon/hosts-ports
+# domains-companies
+mkdir -p $RECON_NG_RECON_MODULES/domains-companies
+cp domains-companies/censys_companies.py $RECON_NG_RECON_MODULES/domains-companies
 
-mkdir -p ~/.recon-ng/modules/recon/netblocks-companies
-cp censys_netblock_company.py ~/.recon-ng/modules/recon/netblocks-companies
+# domains-hosts
+mkdir -p $RECON_NG_RECON_MODULES/domains-hosts
+cp domains-hosts/censys_domain.py $RECON_NG_RECON_MODULES/domains-hosts
 
-mkdir -p ~/.recon-ng/modules/recon/netblocks-hosts
-cp censys_netblock.py ~/.recon-ng/modules/recon/netblocks-hosts
+# hosts-hosts
+mkdir -p $RECON_NG_RECON_MODULES/hosts-hosts
+cp hosts-hosts/censys_query.py $RECON_NG_RECON_MODULES/hosts-hosts
 
-for key in censysio_id censysio_secret; do
-	echo "INSERT INTO keys (name) VALUES (\"$key\");" | sqlite3 ~/.recon-ng/keys.db
-done
+# hosts-ports
+mkdir -p $RECON_NG_RECON_MODULES/hosts-ports
+cp hosts-ports/censys_hostname.py $RECON_NG_RECON_MODULES/hosts-ports
+cp hosts-ports/censys_ip.py $RECON_NG_RECON_MODULES/hosts-ports
+
+# netblocks-companies
+mkdir -p $RECON_NG_RECON_MODULES/netblocks-companies
+cp netblocks-companies/censys_netblock_company.py $RECON_NG_RECON_MODULES/netblocks-companies
+
+# netblocks-hosts
+mkdir -p $RECON_NG_RECON_MODULES/netblocks-hosts
+cp netblocks-hosts/censys_netblock.py $RECON_NG_RECON_MODULES/netblocks-hosts
+
+# add keys
+if [ -f "$RECON_NG_KEYS" ]; then
+	echo "[*] Inserting censys recon-ng keys..."
+	for key in censysio_id censysio_secret; do
+		echo "INSERT OR IGNORE INTO keys (name) VALUES (\"$key\");" | sqlite3 $RECON_NG_KEYS
+	done
+else
+	echo "[-] Please use the 'keys add' command to set 'censysio_id' and 'censysio_secret'."
+fi
+
+exit 0
