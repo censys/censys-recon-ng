adbcfe550e34c47a05a3953b15b13a6f8fb0bdea
Aidan Holland <aidan@censys.io>
2020-10-13 16:35:58 -0400

    chore: update install script

--- install.sh
+++ install.sh
@@ -16,45 +16,46 @@ mkdir -p $RECON_NG_RECON_MODULES
 
 # companies-contacts
 mkdir -p $RECON_NG_RECON_MODULES/companies-contacts
-cp companies-contacts/censys_email_address.py $RECON_NG_RECON_MODULES/companies-contacts
+cp recon/companies-contacts/censys_cert_emails.py $RECON_NG_RECON_MODULES/companies-contacts
 
 # companies-domains
 mkdir -p $RECON_NG_RECON_MODULES/companies-domains
-cp companies-domains/censys_subdomains.py $RECON_NG_RECON_MODULES/companies-domains
+cp recon/companies-domains/censys_cert_domains.py $RECON_NG_RECON_MODULES/companies-domains
 
-# companies-hosts
-mkdir -p $RECON_NG_RECON_MODULES/companies-hosts
-cp companies-hosts/censys_org.py $RECON_NG_RECON_MODULES/companies-hosts
-cp companies-hosts/censys_tls_subjects.py $RECON_NG_RECON_MODULES/companies-hosts
+# companies-multi
+mkdir -p $RECON_NG_RECON_MODULES/companies-multi
+cp recon/companies-multi/censys_asn_org.py $RECON_NG_RECON_MODULES/companies-multi
+cp recon/companies-multi/censys_tls_org.py $RECON_NG_RECON_MODULES/companies-multi
 
 # companies-netblocks
 mkdir -p $RECON_NG_RECON_MODULES/companies-netblocks
-cp companies-netblocks/censys_company_netname.py $RECON_NG_RECON_MODULES/companies-netblocks
+cp recon/companies-netblocks/censys_asn_netname.py $RECON_NG_RECON_MODULES/companies-netblocks
 
 # domains-companies
 mkdir -p $RECON_NG_RECON_MODULES/domains-companies
-cp domains-companies/censys_companies.py $RECON_NG_RECON_MODULES/domains-companies
+cp recon/domains-companies/censys_domain_org.py $RECON_NG_RECON_MODULES/domains-companies
+
+# domains-domains
+mkdir -p $RECON_NG_RECON_MODULES/domains-domains
+cp recon/domains-domains/censys_subdomains.py $RECON_NG_RECON_MODULES/domains-domains
 
 # domains-hosts
 mkdir -p $RECON_NG_RECON_MODULES/domains-hosts
-cp domains-hosts/censys_domain.py $RECON_NG_RECON_MODULES/domains-hosts
+cp recon/domains-hosts/censys_domain.py $RECON_NG_RECON_MODULES/domains-hosts
 
 # hosts-hosts
 mkdir -p $RECON_NG_RECON_MODULES/hosts-hosts
-cp hosts-hosts/censys_query.py $RECON_NG_RECON_MODULES/hosts-hosts
-
-# hosts-ports
-mkdir -p $RECON_NG_RECON_MODULES/hosts-ports
-cp hosts-ports/censys_hostname.py $RECON_NG_RECON_MODULES/hosts-ports
-cp hosts-ports/censys_ip.py $RECON_NG_RECON_MODULES/hosts-ports
+cp recon/hosts-hosts/censys_hostname.py $RECON_NG_RECON_MODULES/hosts-hosts
+cp recon/hosts-hosts/censys_ip.py $RECON_NG_RECON_MODULES/hosts-hosts
+cp recon/hosts-hosts/censys_query.py $RECON_NG_RECON_MODULES/hosts-hosts
 
 # netblocks-companies
 mkdir -p $RECON_NG_RECON_MODULES/netblocks-companies
-cp netblocks-companies/censys_netblock_company.py $RECON_NG_RECON_MODULES/netblocks-companies
+cp recon/netblocks-companies/censys_netblock_company.py $RECON_NG_RECON_MODULES/netblocks-companies
 
 # netblocks-hosts
 mkdir -p $RECON_NG_RECON_MODULES/netblocks-hosts
-cp netblocks-hosts/censys_netblock.py $RECON_NG_RECON_MODULES/netblocks-hosts
+cp recon/netblocks-hosts/censys_netblock.py $RECON_NG_RECON_MODULES/netblocks-hosts
 
 # add keys
 if [ -f "$RECON_NG_KEYS" ]; then
